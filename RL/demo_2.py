c19160cc585914bb16a5f73250e9e3efd3c7c2f9
lukaschen1986 <chen_wen_dong@126.com>
2023-06-04 16:59:02 +0800

    modify RL

--- RL/demo_2.py
+++ RL/demo_2.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-https://www.bilibili.com/video/BV1R84y1T7s8/?spm_id_from=333.999.0.0&vd_source=fac9279bd4e33309b405d472b24286a8
+https://www.bilibili.com/video/BV1Yo4y1n7om/?spm_id_from=333.880.my_history.page.click&vd_source=fac9279bd4e33309b405d472b24286a8
 """
 import os
 import copy
@@ -15,7 +15,7 @@ path_data = os.path.join(os.path.dirname(path_project), "data")
 path_model = os.path.join(os.path.dirname(path_project), "model")
 
 # ----------------------------------------------------------------------------------------------------------------
-# 策略迭代
+# 价值迭代
 """
 A
 0: 左
@@ -36,56 +36,36 @@ env.render()
 n_states = env.observation_space.n  # 状态集合
 n_actions = env.action_space.n  # 动作集合
 arr_v = np.zeros(n_states)  # 每个状态s所对应的价值量
-arr_pi = np.random.randint(low=0, high=n_actions, size=n_states, dtype=int)  # 每个状态s所对应的策略，初始化随机性策略
-arr_q = np.zeros([n_states, n_actions])
+arr_pi = np.zeros(n_states, dtype=int)  # 每个状态s所对应的策略
+print(arr_v)
+print(arr_pi)
 
-# ----------------------------------------------------------------------------------------------------------------
-# 策略迭代：策略评估（pi -> v）
+# 价值迭代
 gamma = 0.9
 n_iters = 1000
 eps = 10**-8
 
-def policy_evaluation(arr_pi):
-    for i in range(n_iters):
-        prev_arr_v = copy.deepcopy(arr_v)
-        
-        # 遍历状态索引（S = s）
-        for s in range(n_states):
-            a = arr_pi[s]  # 获取确定性策略
-            v = 0
-            for (prob, next_s, r, flag) in env.P[s][a]:
-                v += prob * (r + gamma * prev_arr_v[next_s])  # 1.2-对应v(s)的积分公式
-            arr_v[s] = v  # 1.2-贝尔曼期望方程
-        
-        if np.max(np.abs(prev_arr_v - arr_v)) < eps:
-            break
+for i in range(n_iters):
+    prev_arr_v = copy.deepcopy(arr_v)
     
-    return arr_v
-
-# ----------------------------------------------------------------------------------------------------------------
-# 策略迭代：策略改进（q -> pi'）
-def policy_update(arr_v):
     # 遍历状态索引（S = s）
     for s in range(n_states):
+        lst_q = []  # 给定s，会产生n_actions个q，依次存入lst_q
+        
         # 遍历动作索引（A = a）
         for a in range(n_actions):
-            for (prob, next_s, r, flag) in env.P[s][a]:
-                arr_q[s, a] += prob * (r + gamma * arr_v[next_s])  # 1.3-对应q(s, a)的积分公式
+            q = 0  # 给定s和a，会产生一组q
+            for (prob, next_s, r, flag) in env.P[s][a]:  # env.P为状态转移矩阵；prob为动态特性
+                q += prob * (r + gamma * prev_arr_v[next_s])  # 1.3-对应q(s, a)的积分公式
+            lst_q.append(q)
+        
+        arr_v[s] = np.max(lst_q)  # 1.3-贝尔曼最优方程
+        arr_pi[s] = np.argmax(lst_q)  # 2.4-贪心策略
     
-    arr_pi = np.argmax(arr_q, axis=1)  # 2.4-贪心策略
-    return arr_pi
-
-# ----------------------------------------------------------------------------------------------------------------
-# 策略迭代
-while True:
-    arr_v = policy_evaluation(arr_pi)
-    arr_pi_new = policy_update(arr_v)
-    
-    if np.array_equal(arr_pi_new, arr_pi):
+    if np.max(np.abs(prev_arr_v - arr_v)) < eps:
         break
-    else:
-        arr_pi = arr_pi_new
 
+print(arr_v)
 print(arr_pi.reshape(4, 4))
 
 # 测试
@@ -99,7 +79,20 @@ while not flag:
     next_s, r, flag, truncated, info = env.step(a)
     env.render()
     print(s)
-        
-        
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
