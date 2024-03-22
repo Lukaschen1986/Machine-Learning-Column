4f8473bddcda68c7ad9d26983312047452e55103
lukaschen1986 <chen_wen_dong@126.com>
2024-03-22 21:07:13 +0800

    add vscode

---
+++ tools/demo_vscode.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+https://www.bilibili.com/video/BV1TN411K7sn/?p=3&spm_id_from=333.880.my_history.page.click&vd_source=fac9279bd4e33309b405d472b24286a8
+https://www.bilibili.com/video/BV1TN411K7sn?p=4&vd_source=fac9279bd4e33309b405d472b24286a8
+https://www.bilibili.com/video/BV1Bg411J78F/?spm_id_from=333.880.my_history.page.click&vd_source=fac9279bd4e33309b405d472b24286a8
+"""
+print(123)
+
+# python-snippets
+def fname(arg):
+    pass
+
+def mname(self, arg):
+    pass
+
+class ClassName(object):
+    """docstring for ClassName."""
+    def __init__(self, arg):
+        super(ClassName, self).__init__()
+
+
+if __name__ == "__main__":
+    main()
