094074d5069d0b8f43779f8768585f47142ae71a
Lukas_Chen <chen_wen_dong@126.com>
2025-03-02 21:03:41 +0800

    Add files via upload

---
+++ python_adv/demo_10_abc.py
@@ -0,0 +1,21 @@
+from abc import (ABC, abstractclassmethod)
+
+
+class Course(ABC):
+    @staticmethod
+    def enroll(self, student):
+        print(f"{student} enrolled in Course")
+
+class MathCourse(Course):
+    def enroll(self, student):
+        print(f"{student} enrolled in MathCourse") 
+
+class ProgrammCourse(Course):
+    def enroll(self, student):
+        print(f"{student} enrolled in ProgrammCourse") 
+
+if __name__ == "__main__":
+    courses = [MathCourse(), ProgrammCourse()]
+    for course in courses:
+        course.enroll("Alice")
+
