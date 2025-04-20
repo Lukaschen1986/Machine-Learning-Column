dc915d5de5a2ff7c62c48c59ce6481ca89ab39d4
lukaschen1986 <chen_wen_dong@126.com>
2025-04-20 20:13:03 +0800

    modify llm

--- python_adv/demo_11_nametuple.py
+++ python_adv/demo_11_nametuple.py
@@ -1,20 +1,21 @@
 from collections import namedtuple
 from typing import NamedTuple
 
+
 # 常规方法
-p1 = {"x": 1, "y": 2}
-p2 = {"x": 2, "y": 3}
+class Point(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+# typing.NamedTuple 方法
+class Point(NamedTuple):
+    x: int
+    y: int
 
 # collections.nametuple 方法
 Point = namedtuple(typename="Point", field_names=["x", "y"])
+
 p1 = Point(x=1, y=2)
 print(p1.x)  # 1
-print(type(p1.x))
 
-# typing.NamedTuple 方法
-# class Point(NamedTuple):
-#     x: int
-#     y: int
-
-# p1 = Point(x=1, y=2)
-# print(p1.x)  # 1
