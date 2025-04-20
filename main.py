23d8c0b837cd40b69e60d858f9ebca57d9154b9d
lukaschen1986 <chen_wen_dong@126.com>
2025-04-20 12:01:29 +0800

    add nametuple

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import python_adv
+# from python_adv.demo_01 import Student  # ok
+from python_adv import Student  # __init__.py  from .demo_01 import Student
 
 print(python_adv.__version__)
 print(python_adv.__author__)
+
+
+
--- python_adv/__init__.py
+++ python_adv/__init__.py
@@ -1,2 +1,4 @@
+from .demo_01 import Student
+
 __version__ = "1.0.0"
 __author__ = "lukas-i33"
