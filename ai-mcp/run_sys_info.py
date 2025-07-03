e98f4b6cd273619086da5af2bcbf7568b45035f4
Lukaschen1986 <chen_wen_dong@126.com>
2025-07-03 19:47:16 +0800

    modify mcp

--- ai-mcp/run_sys_info.py
+++ ai-mcp/run_sys_info.py
@@ -6,6 +6,7 @@ from mcp.server.fastmcp import FastMCP
 
 
 mcp = FastMCP(name="sysInfo")
+# mcp = FastMCP(name="sysInfo", port=8080)  # for sse
 mcp.add_tool(fn=tools_sys_info.get_host_info)
 # mcp.add_tool(fn=...)  # 可以添加多个工具函数
 
@@ -17,6 +18,7 @@ mcp.add_tool(fn=tools_sys_info.get_host_info)
 def main():
     print("Hello there!")
     mcp.run(transport="stdio")
+    # mcp.run(transport="sse")  # http://127.0.0.1:8080/sse
 
 
 if __name__ == "__main__":
